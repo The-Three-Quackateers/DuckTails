6fae508bc0538600fdacadb897a0f81c49058a43
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-02-24 11:53:29 -0500

    Randomized the coins spawning

--- Javascript/Coins.js
+++ Javascript/Coins.js
@@ -1,5 +1,8 @@
 let gameScene = new Phaser.Scene('Game')
 
+let coinsSpawn = 7
+let coinArray = []
+
 let config = {
     type:Phaser.AUTO,
     width: 800,
@@ -25,21 +28,36 @@ function create(){
     bg.setOrigin(0,0)
     bg.setScale(0.6,0.52)
     let duck = this.add.sprite(0,0,'Player')
-    let ycordcoins = Phaser.Math.Between(50,500)
-    let xcordcoins = Phaser.Math.Between(50,800)
-    this.add.sprite(xcordcoins,ycordcoins,'Coins')
+    
 
     let ycordbread = Phaser.Math.Between(50,500)
     let xcordbread = Phaser.Math.Between(50,800)
     this.add.sprite(xcordbread,ycordbread,'Bread')
+    for(let i = 0;i<coinsSpawn;i++) {
+        let ycordcoins = Phaser.Math.Between(50,500)
+        let xcordcoins = Phaser.Math.Between(50,800)
+        let coin = this.add.sprite(xcordcoins,ycordcoins,'Coins')
+        coinArray.push(coin)
 
-
+    }
+    console.log(coinArray)
+    //Changing this changes how long until the coins spawn
+    setInterval(coinRandom,15000)
     duck.setScale(0.2,0.2)
     duck.setOrigin(0,0)
     duck.displayWidth = 100
     duck.displayHeight = 100
+    
+}
+//Function to control the coins spawning at random
+function coinRandom() {
+    for(let i = 0;i<coinArray.length;i++) {
+        let ycordcoins = Phaser.Math.Between(50,500)
+        let xcordcoins = Phaser.Math.Between(50,800)
+        coinArray[i].x = xcordcoins
+        coinArray[i].y = ycordcoins
+        console.log(coinArray[i])
+    }
 }
 
-function update() {
-    console.log('up')
-}
+function update() {}
