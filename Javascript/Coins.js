7ce0c9ffa923b4beada81e3c957f94d8dbff99cb
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-02-24 14:14:47 -0500

    Changed around positions of the background and duck scale

--- Javascript/Coins.js
+++ Javascript/Coins.js
@@ -1,18 +1,27 @@
 let gameScene = new Phaser.Scene('Game')
 
-let coinsSpawn = 7
+
+
+let coinsSpawn = 15
 let coinArray = []
 
 let config = {
     type:Phaser.AUTO,
-    width: 800,
-    height: 500,
+    width: 1214,
+    height: 800,
     scene: gameScene = {
         update:update,
         create:create,
         preload:preload,
-    }
-}
+    },
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug:true
+        }
+    },
+
+}  
 
 let game = new Phaser.Game(config)
 
@@ -26,19 +35,20 @@ function preload() {
 function create(){
     let bg = this.add.sprite(0,0,'background')
     bg.setOrigin(0,0)
-    bg.setScale(0.6,0.52)
     let duck = this.add.sprite(0,0,'Player')
-    
+    console.log(this.physics)
 
     let ycordbread = Phaser.Math.Between(50,500)
     let xcordbread = Phaser.Math.Between(50,800)
-    this.add.sprite(xcordbread,ycordbread,'Bread')
+
+    let bread = this.add.sprite(xcordbread,ycordbread,'Bread')
+    bread.setScale(0.5)
     for(let i = 0;i<coinsSpawn;i++) {
         let ycordcoins = Phaser.Math.Between(50,500)
         let xcordcoins = Phaser.Math.Between(50,800)
         let coin = this.add.sprite(xcordcoins,ycordcoins,'Coins')
         coinArray.push(coin)
-
+        coin.setScale(0.2)
     }
     console.log(coinArray)
     //Changing this changes how long until the coins spawn
